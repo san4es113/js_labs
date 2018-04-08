569e3d93573c57514273d41293088c1a2eb15ff0
dimon <dmytroroik@gmail.com>
2018-04-08 22:53:40 +0300

    +validation email, +hide animation

--- src/components/LogingPage/LogingPage.jsx
+++ src/components/LogingPage/LogingPage.jsx
@@ -2,15 +2,25 @@ import React from 'react';
 import './LogingPage.css';
 
 const logingPage=(props)=>(
-    <div className="LogingPage">
-    <form >
+    <div className={"LogingPage" + (props.show? "":" hide") }>
+    <form onSubmit={props.onSubmitData}>
         <label htmlFor="userEmail">Email:</label>
-        <input type="email" id="userEmail" placeholder="usermail@example.com"/>
+        <input 
+            type="email" 
+            id="userEmail" 
+            placeholder="usermail@example.com"
+            onInput={props.onInputData}
+            />
 
         <label htmlFor="userPassword">Password:</label>
-        <input type="password" id="userPassword" placeholder="password"/>
+        <input 
+            type="password" 
+            id="userPassword" 
+            placeholder="password"
+            onInput={props.onInputData}
+            />
         
-        <input type="submit" value="Enter"/>
+        <input type="submit" value="Enter" />
     </form>
     </div>
 );
--- src/containers/StartPage/StartPage.jsx
+++ src/containers/StartPage/StartPage.jsx
@@ -2,12 +2,42 @@ import React,{Component} from 'react';
 import LogingPage from '../../components/LogingPage/LogingPage';
 
 class StartPage extends Component{
+    constructor(props){
+        super(props);
+        this.state={
+            showPage:true
+        }
+        this.userData={
+            email:'',
+            password:''
+        }
+    }
+    onInputUserData = (e)=>{
+        if(e.target.id ==='userEmail' ){
+            this.userData.email= e.target.value;
+        } else if (e.target.id === 'userPassword') {
+            this.userData.password = e.target.value;
+        }
+    }
+    onSubmitData = (e) => {
+        // make ajax to server
+        e.preventDefault();
+        alert(`email:${this.userData.email}, password:${this.userData.password}`);
+        this.setState({showPage: false});
+    }
+
     render(){
         return (
             <div>
-                <LogingPage/>
+                <LogingPage 
+                    show = {this.state.showPage}
+                    onInputData = {this.onInputUserData}
+                    onSubmitData = {this.onSubmitData}/>
             </div>
         );
+    }
+    componentDidMount(){
+   
     }
 }
 export default StartPage;
